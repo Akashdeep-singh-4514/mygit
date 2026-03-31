863e1d2438335a4db7d663307f2c345b7274b5e4
Akashdeep Singh <waliaakash95@gmail.com>
2026-03-31 18:12:01 +0530

    updating add flow

--- src/index/manage.rs
+++ src/index/manage.rs
@@ -1,15 +1,29 @@
-use std::fs::OpenOptions;
-use std::io;
-use std::io::Write;
+use std::fs;
+use std::io::{self, Write};
 
 pub fn update_index(file: &str, hash: &str) -> io::Result<()> {
     let index_path = ".mygit/index";
 
-    let mut file_handle = OpenOptions::new()
-        .create(true)
-        .append(true)
-        .open(index_path)?;
+    let mut entries = Vec::new();
 
-    writeln!(file_handle, "{} {}", hash, file)?;
+    if let Ok(content) = fs::read_to_string(index_path) {
+        for line in content.lines() {
+            let mut parts = line.split_whitespace();
+            let existing_hash = parts.next().unwrap();
+            let existing_file = parts.next().unwrap();
+
+            if existing_file != file {
+                entries.push(format!("{} {}", existing_hash, existing_file));
+            }
+        }
+    }
+
+    entries.push(format!("{} {}", hash, file));
+
+    let mut file_handle = fs::File::create(index_path)?;
+    for entry in entries {
+        writeln!(file_handle, "{}", entry)?;
+    }
+
     Ok(())
 }
